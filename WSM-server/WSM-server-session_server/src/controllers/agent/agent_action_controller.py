a6659d4e0c94782dda230be3d3a6b4edb326dc38
Jean M Santos <jean.michel@ebz.tec.br>
2024-12-04 15:02:41 -0300

    added agent notification endpoint

---
+++ WSM-server/WSM-server-session_server/src/controllers/agent/agent_action_controller.py
@@ -0,0 +1,23 @@
+from fastapi import HTTPException, status
+
+from src.config import config
+from src.config.wsm_logger import logger
+from src.models.schema.agent_model import AgentActionSchema
+from src.services.rabbitmq.rabbitmq_send_message_service import RabbitMQSendMessageService
+
+
+class AgentActionController:
+
+    @staticmethod
+    async def execute(payload: AgentActionSchema):
+        try:
+            rabbitmq_send_message = RabbitMQSendMessageService(queue_name=config.WSM_AGENT_NOTIFICATION_QUEUE)
+
+            message = payload.model_dump()
+            rabbitmq_send_message.send(message)
+        except Exception as e:
+            logger.error(f"Could not send action {payload.model_dump()}, reason: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Could not send action {payload.model_dump()}, reason: {e}"
+            )
