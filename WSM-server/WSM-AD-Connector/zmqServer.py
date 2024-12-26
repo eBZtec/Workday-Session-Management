cdbf6805b68a281419d4be9d55d6bad1ab04de7a
root <root@wsm.safra.lab>
2024-12-26 14:38:29 -0300

    Logger write line is removed in WSM-AD-Connector, because is writing inside a loop.

--- WSM-server/WSM-AD-Connector/zmqServer.py
+++ WSM-server/WSM-AD-Connector/zmqServer.py
@@ -59,8 +59,6 @@ class RabbitMQReader:
             self.data = json.loads(body.decode('utf-8'))
             self.data.pop('_sa_instance_state', None)
             logger.info(f"Message consumed from RabbitMQ: {self.data}")
-        else:
-            logger.warning("No messages in the queue.")
         return self.data
 
 # Generate a random AES key and IV
