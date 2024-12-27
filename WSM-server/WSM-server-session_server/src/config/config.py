66227db465434ebe9f77fc75206c84095ea180a2
Jean M Santos <jean.michel@ebz.tec.br>
2024-12-27 15:56:53 -0300

    using credentials to authenticate on rabbitMQ server

--- WSM-server/WSM-server-session_server/src/config/config.py
+++ WSM-server/WSM-server-session_server/src/config/config.py
@@ -6,18 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configurações gerais
-DATABASE_URL = os.getenv("DEV_DATABASE_URL")
-MQ_ADDRESS_HOST = os.getenv("DEV_MQ_ADDRESS_HOST")
-MQ_HOST_PORT = os.getenv("DEV_MQ_HOST_PORT")
-WORK_HOURS_QUEUE= os.getenv('DEV_WORK_HOURS_QUEUE')
+DATABASE_URL = os.getenv("DATABASE_URL")
+MQ_ADDRESS_HOST = os.getenv("MQ_ADDRESS_HOST")
+MQ_HOST_PORT = os.getenv("MQ_HOST_PORT")
+MQ_USER = os.getenv("MQ_USER")
+MQ_PASSWORD = os.getenv("MQ_PASSWORD")
+WORK_HOURS_QUEUE= os.getenv('WORK_HOURS_QUEUE')
 LOGOFF_QUEUE = "logoff"
 WSM_AGENT_NOTIFICATION_QUEUE = os.getenv('WSM_AGENT_NOTIFICATION_QUEUE')
 
-ZEROMQ_URL = os.getenv("DEV_ZEROMQ_URL")
-
 OAUTH_SECRET_KEY = os.getenv("OAUTH_VALID_SECRET_KEY")
 
-LOG_FILE = "./logs/wsm.log"
+LOG_FILE = os.getenv("WSM_LOG_PATH")
 LOG_LOGGER = "WSM Logger"
 LOG_LEVEL = logging.DEBUG
 LOG_MAX_BYTES = 100*1024*1024
@@ -25,10 +25,3 @@ LOG_BACKUP_COUNT = 14
 
 if not DATABASE_URL:
     raise ValueError("A variável de ambiente DATABASE_URL não está definida.")
-
-
-
-
-
-
-
