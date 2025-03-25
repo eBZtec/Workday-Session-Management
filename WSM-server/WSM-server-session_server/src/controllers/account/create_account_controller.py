e3cdeaa8e87b7aa986fc62cce008c4ba3d7eb716
Jean M Santos <jean.michel@ebz.tec.br>
2025-03-25 10:24:09 -0300

    fix: update account endpoint must create account if it not exists

--- WSM-server/WSM-server-session_server/src/controllers/account/create_account_controller.py
+++ WSM-server/WSM-server-session_server/src/controllers/account/create_account_controller.py
@@ -2,6 +2,7 @@
 from src.config.wsm_logger import logger
 from src.models.schema.request_models import StandardWorkHoursSchema
 from src.services.account.calculate_account_workhours_service import CalculateWorkhoursService
+from src.services.account.create_account_and_targets_service import CreateAccountAndTargetsService
 from src.services.account.insert_account_database_service import InsertAccountDatabaseService
 from src.services.account.search_account_by_uid_service import SearchAccountByUIDService
 from src.services.pooling.accounts_pooling_service import AccountsPoolingService
@@ -14,17 +15,7 @@ class CreateAccountController:
     @staticmethod
     async def execute(standard_work_hours: StandardWorkHoursSchema):
         try:
-            uid = standard_work_hours.uid
-            account_data = standard_work_hours.model_dump()
-            await InsertAccountDatabaseService.execute(standard_work_hours)
-
-            account = await SearchAccountByUIDService.execute(uid)
-
-            if account:
-                logger.info(f"Entry {account_data} added successfully in the database")
-                await ConfigureAccountTargets.execute(account.id)
-            else:
-                raise Exception(f"Account \"{uid} not inserted into database.")
+            uid = await CreateAccountAndTargetsService.execute(standard_work_hours)
         except Exception as e:
             logger.error(f"Could not insert entry {standard_work_hours.model_dump()} on database, reason: {e} ")
         else:
