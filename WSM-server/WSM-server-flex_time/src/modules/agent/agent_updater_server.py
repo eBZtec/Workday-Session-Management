72117949d1bffc2a77646a483aa83fd14c939c16
Jean M Santos <jean.michel@ebz.tec.br>
2025-04-15 18:44:41 -0300

    defined flex time calculator for allowed logon hours, zeromq reply server

--- WSM-server/WSM-server-flex_time/src/modules/agent/agent_updater_server.py
+++ WSM-server/WSM-server-flex_time/src/modules/agent/agent_updater_server.py
@@ -1,39 +1,53 @@
+import json
+
+import zmq
+
+from src.config.wsm_config import wsm_config
 from src.config.wsm_logger import wsm_logger
 from src.infra.wsm_session_database import wsm_session_database
-from src.shared.enums.types import JourneyType, WorkTimeType
+from src.infra.wsm_zeromq_manager import wsm_zeromq_manager
+from src.shared.enums.types import JourneyType
 from src.shared.services.account.search_account_service import SearchAccountService
+from src.shared.services.flextime.calculate_flextime_service import CalculateFlextimeService
 
 
 class AgentUpdaterServer:
     @staticmethod
     def start():
-        uid = "teste14"
-        with wsm_session_database.session_scope() as session:
-            search_account_service = SearchAccountService(session)
 
-            accounts = search_account_service.get_account_by_uid(uid)
-
-            if len(accounts) == 1:
-                account = accounts[0]
-                wsm_logger.info(f"Found one account for uid {account.uid}")
-
-                if account.journey == JourneyType.FLEX_TIME:
-                    wsm_logger.debug(f"Processing uid {account.uid} with journey defined as {account.journey}")
-                    last_flex_time = search_account_service.get_last_flex_time_by_account_id(account.id)
-
-                    if last_flex_time:
-                        wsm_logger.info(f"Found last flex time for uid {account.uid} (work time \"{last_flex_time.work_time}\", type \"{last_flex_time.work_time_type}\")")
-                        wsm_logger.debug(f"Last flex time found as {last_flex_time.as_dict()}")
-
-                        if last_flex_time.work_time_type == WorkTimeType.OUT:
-                            wsm_logger.info(f"User {account.uid} is not able to logon. Blocking user work hours....")
-                        else:
-                            wsm_logger.info(f"User {account.uid} is able to logon. Calculating work hours....")
-                    else:
-                        wsm_logger.info(f"No flex time found for uid {account.uid}, user work time must be blocked.")
-                else:
-                    wsm_logger.warning(f"Skipping processing, reason: Journey defined as {account.journey} for uid {account.uid}")
-            elif len(accounts) > 1:
-                wsm_logger.warning(f"Found more the one account for uid {uid}")
+        while True:
+            try:
+                wsm_logger.info(f"Waiting message on ZeroMQ \"{wsm_config.wsm_zeromq_url}\" host...")
+                uid = wsm_zeromq_manager.get_socket().recv_string()
+                wsm_logger.info(f"Received ZeroMQ message \"{uid}\"")
+            except zmq.ZMQError as e:
+                wsm_logger.error(f"WSM - simple_route_server_service - ZQM error: {e}")
+            except Exception as e:
+                wsm_logger.error(f"WSM - simple_route_server_service - ZQM error: {e}")
             else:
-                wsm_logger.warning(f"No account was found for uid {uid}")
+                with wsm_session_database.session_scope() as session:
+                    search_account_service = SearchAccountService(session)
+
+                    accounts = search_account_service.get_account_by_uid(uid)
+
+                    if len(accounts) == 1:
+                        account = accounts[0]
+                        wsm_logger.info(f"Found one account for uid \"{account.uid}\"")
+
+                        if account.journey == JourneyType.FLEX_TIME:
+                            calculate_work_hours_service = CalculateFlextimeService(account, session)
+                            processed_account = calculate_work_hours_service.calculate()
+
+                            data = {k: v for k, v in processed_account.__dict__.items() if not k.startswith('_')}
+                            response = json.dumps(data, default=str)
+
+                            wsm_logger.info(f"Account update response for agent {response}")
+
+                            wsm_zeromq_manager.get_socket().send_string(response)
+                            wsm_logger.info("Message sent successfully for ZeroMQ host")
+                        else:
+                            wsm_logger.warning(f"Skipping processing, reason: Journey defined as {account.journey} for uid {account.uid}")
+                    elif len(accounts) > 1:
+                        wsm_logger.warning(f"Found more the one account for uid {uid}")
+                    else:
+                        wsm_logger.warning(f"No account was found for uid {uid}")
