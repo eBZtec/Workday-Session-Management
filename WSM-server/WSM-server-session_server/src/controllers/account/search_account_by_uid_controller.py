84f0aa3b2fd64856c866e0278a7075ff1ef89a06
Jean M Santos <jean.michel@ebz.tec.br>
2025-04-29 15:05:36 -0300

    formatted work hours

--- WSM-server/WSM-server-session_server/src/controllers/account/search_account_by_uid_controller.py
+++ WSM-server/WSM-server-session_server/src/controllers/account/search_account_by_uid_controller.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, status
 
 from src.config.wsm_logger import logger
+from src.services.account.presenter.format_work_hours_service import FormatWorkHoursService
 from src.services.account.presenter.search_account_by_uid_service import SearchAccountByUIDService
 from src.services.overtime.get_overtimes_by_account_uid_service import GetOvertimesByAccountUidService
 from src.services.targets.get_target_status_by_account_uid_service import GetTargetStatusByAccountUidService
@@ -36,7 +37,9 @@ class SearchAccountByUidController:
                         targets.append(target_status_response)
 
                     account_targets = {"target_status": targets}
+                    formatted_work_hours = {"formatted_work_hours": await FormatWorkHoursService.format(account.allowed_work_hours)}
 
+                    clean_account_data.update(formatted_work_hours)
                     clean_account_data.update(account_targets)
 
                 return clean_account_data
