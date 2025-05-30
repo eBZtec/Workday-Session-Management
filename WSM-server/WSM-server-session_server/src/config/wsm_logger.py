8705f65d68ec97dea9e2c05460c50aea5993d255
root <root@localhost.localdomain>
2025-05-30 13:30:50 -0300

    Added into session_server logger into stdout and the funcionality of agent_updater post into session_agent queue a message

--- WSM-server/WSM-server-session_server/src/config/wsm_logger.py
+++ WSM-server/WSM-server-session_server/src/config/wsm_logger.py
@@ -1,13 +1,23 @@
 import os
-import logging.handlers
-
+import logging
+import logging.handlers 
 from src.config import config
 
+try:
+    from systemd.journal import JournalHandler
+    HAS_JOURNAL = True
+except:
+    HAS_JOURNAL = False
 
+# Log path
 cwd = os.path.dirname(os.path.abspath(__file__))
 log_file = config.LOG_FILE
+
+# Main logger
 logger = logging.getLogger(config.LOG_LOGGER)
 logger.setLevel(config.LOG_LEVEL)
+
+# Log rotate handler
 log_handle = logging.handlers.RotatingFileHandler(
     log_file,
     maxBytes=config.LOG_MAX_BYTES,
@@ -18,3 +28,12 @@ log_format = logging.Formatter('%(asctime)s - %(levelname)s - Work Session Manag
 log_handle.setFormatter(log_format)
 logger.addHandler(log_handle)
 
+# Optional handler to journalctl
+if HAS_JOURNAL:
+    journal_handler = JournalHandler()
+    journal_handler.setLevel(config.LOG_LEVEL)
+    journal_formatter = logging.Formatter('%(levelname)s - Work Session Management - %(message)s')
+    journal_handler.setFormatter(journal_formatter)
+    logger.addHandler(journal_handler)
+else:
+    logger.warning("systemd.journal.JournalHandler not available. Log to journalctl deactivated.")
